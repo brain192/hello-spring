2bd73a9b2a169390fc0ef97f78093f5a41a213bf
KJH <brain192@naver.com>
2024-11-22 17:31:07 +0900

    13 컴포넌트스캔 진행중, 12파트 문제해결

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +12,25 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    //MemberService memberService = new MemberService();
     //test생성법 테스트 하고자 하는 부분에서 ctrl+shift+t를 누른 후 메뉴에서 체크
     //@test는 test 실행 가능하게 하는 함
     //테스트는 한글로 사용해도 됨 ex) void 회원가입()
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+   // MemoryMemberRepository memberRepository = new MemoryMemberRepository();
     //클리어를 해주기 위해 클리어를 생성한 memberRepository 사용
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
 
     @BeforeEach
     public void beforeEach(){
         memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
     }
 
     @Test
@@ -50,11 +59,11 @@ class MemberServiceTest {
 
         Member member2 = new Member();
         member2.setName("spring");
-        //when
+        //When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () ->  memberService.join(member2));
-
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        //assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
          /*
         try {
