1daf6f86fa12c169b97e0e3bd5946d1471547050
KJH <brain192@naver.com>
2024-11-26 23:42:42 +0900

    15 진행중

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -16,5 +17,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
